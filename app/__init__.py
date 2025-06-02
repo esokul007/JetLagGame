5daf7acf67dba6f587c062e05d4a9b8a5d935a8a
Jacob Lukose <esokul007@gmail.com>
2025-06-02 19:34:27 +0000

    Cards basis

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import random, sys, os, sqlite3
-import cards
+import cards, db
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	press = False
-	card = ""
-	if request.method == 'POST':
+        if 'username' not in session:
+                session['username'] = ""
+                session['all_cards'] = ""
+                session['drawn'] =""
+        else:
+                session['all_cards'] = db.getCards(session['username'])
+        username = session['username']
+        all_cards= session['all_cards']
+        press = False
+        card = session['drawn']
+        if request.method == 'POST':
                 press = request.form.get('random')
-                card = cards.select()
-	return render_template("home.html", card = card, press = press)
+                if request.form.get('add'):
+                        if isinstance(card, str):
+                                db.addCard(username,card)
+                        else:
+                                db.addCard(username, card[0])
+                if press:
+                        card = cards.select()
+                        session['drawn'] = card
+                if request.form.get('user'):
+                        username = request.form['username']
+                        session['username'] = username
+                        db.addUser(username)
+                if request.form.get('rm'):
+                        rm_card = request.form['remove']
+                        db.remCard(username, rm_card)
+                all_cards = db.getCards(username)
+                session['all_cards'] = all_cards
+        #print(username)
+        return render_template("home.html", card = card, press = press, username = username, all_cards = all_cards)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
---
+++ app/db.py
@@ -0,0 +1,61 @@
+import sqlite3
+import csv
+from datetime import datetime
+
+DB_FILE = "players.db"
+        
+def setup():            
+	db = sqlite3.connect(DB_FILE, check_same_thread=False)
+	c = db.cursor()
+	c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT NOT NULL UNIQUE, card1 TEXT, card2 TEXT,card3 TEXT,card4 TEXT,card5 TEXT);")   
+ # c.execute("CREATE TABLE IF NOT EXISTS gameChallenge (challenge_ID INTEGER PRIMARY KEY AUTOINCREMENT, chal>
+ # c.execute("CREATE TABLE IF NOT EXISTS gameHistory (game_ID INTEGER PRIMARY KEY, winner TEXT, loser TEXT);>
+ # c.execute("CREATE TABLE IF NOT EXISTS gameTracker (game_ID INTEGER, player1 TEXT, player2 TEXT, move1 TE
+
+	db.commit()
+	db.close()
+
+def addUser(user): 
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)         
+    c = db.cursor()
+    c.execute("INSERT or IGNORE INTO users (username) VALUES (?)",(user,))
+    db.commit()
+    db.close()
+
+def addCard(user, card):
+    index=4
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)                       
+    c = db.cursor()
+    c.execute("SELECT card1, card2, card3, card4, card5 FROM users WHERE username = (?)", (user,))
+    result=c.fetchone()
+    for i in range(len(result)):
+        if result[i] == None:
+            index=i
+            break
+    c.execute(f"UPDATE users SET card{index + 1} = ? WHERE username = ?", (card, user))
+    print(result)
+    db.commit()
+    db.close()  
+
+def remCard(user, card):
+	index=4
+	db = sqlite3.connect(DB_FILE, check_same_thread=False)
+	c = db.cursor()
+	c.execute("SELECT card1, card2, card3, card4, card5 FROM users WHERE username = (?)", (user,))
+	result=c.fetchone()
+	for i in range(len(result)):
+		if result[i] ==card:
+			index=i
+			break
+	c.execute(f"UPDATE users SET card{index + 1} = ? WHERE username = ?", (None, user))
+	print(result)
+	db.commit()
+	db.close()
+def getCards(user):
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)
+    c = db.cursor()
+    c.execute("SELECT card1, card2, card3, card4, card5 FROM users WHERE username = (?)", (user,))
+    result=c.fetchone()
+    return result
+
+remCard("Eastwood", "25")
