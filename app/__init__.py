0242a2e28abb5084cc63966fa5446027ed631dc9
esokul007 <esokul007@gmail.com>
2025-06-07 08:24:23 -0400

    swipe

--- app/__init__.py
+++ app/__init__.py
@@ -92,6 +92,7 @@ def home():
                 return redirect(url_for('home'))
         #print(username)
         session['show'] = False
+        print(cursed)
         return render_template("home.html", card = card, show = show, username = username, all_cards = all_cards, questions = questions, draws= draws, question = question, picks = picks, allUsers= all_users, sentAt = sentAt, allowed_time = 300, cursed = cursed)
 
 if __name__ == '__main__':
